8cf16263220628f29b0804579fc064e6e0924684
Sven Haider <sven.haider@web.de>
2020-07-21 11:15:18 +0200

    fixed js toggle-effect

---
+++ app/javascript/components/toggle_nutrients.js
@@ -0,0 +1,26 @@
+const toggleNutrients = () => {
+  const foods = Array.from(document.querySelectorAll('.toggle-nutrients'));
+  const foodNutrients = Array.from(document.querySelectorAll('.nutrients'));
+  
+  foods.forEach((food) => {
+    foodNutrients.forEach((nutrient) => {
+      food.addEventListener('click', () => {
+        
+        
+        if (foods.indexOf(food) === foodNutrients.indexOf(nutrient)) {
+          if (nutrient.classList.contains('hide')) {
+            nutrient.classList.remove('hide')
+            nutrient.classList.add('show')
+          } else {
+            nutrient.classList.remove('show')
+            nutrient.classList.add('hide')
+          };
+        };
+        // console.log(foods.indexOf(food));
+        // console.log(foodNutrients.indexOf(nutrient));
+      });
+    });
+  });
+};
+
+export { toggleNutrients }
